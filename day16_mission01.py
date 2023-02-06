7bdc8243de7549e8a352ad2504ac09c02af3d4f5
KimTaeYeong <12170548@inha.edu>
2023-02-06 22:53:15 +0900

    day16_mission02

--- day16_mission01.py
+++ day16_mission01.py
@@ -1,82 +1,50 @@
-import random
-import math
-
-
-## 클래스와 함수 선언 부분 ##
-class Node():
-    def __init__(self):
+# 문제 2
+class Node() :
+    def __init__ (self) :
         self.data = None
-        self.link = None
+        self.nlink = None
+        self.plink = None
 
-
-def print_store(start):
+def printNodes(start):
     current = start
-    if current.link == head:
+    if current.nlink == None : #None 값이 있다면 선형 리스트에서는 정지를 해야되니까
         return
-
-    while current.link != start:
-        current = current.link
-        x = current.data[1]
-        y = current.data[2]
-        print(current.data[0], '편의점 거리: ', math.sqrt(x * x + y * y))
+    print("정방향 ")
+    print(current.data, end=' ')
+    while current.nlink != None:
+        current = current.nlink
+        print(current.data, end=' ')
     print()
 
+    print("역방향 ")
+    print(current.data, end=' ')
 
-def sort_store(store_name):
-    global memory, head, current, pre
+    while current.plink != None:
+        current = current.plink
+        print(current.data, end=' ')
+    print()
 
-    node = Node()
-    node.data = store_name
-    memory.append(node)
-
-    if head == None:
-        head = node
-        node.link = head
-        return
-
-    node_reach = math.sqrt(node.data[1] * node.data[1] + node.data[2] * node.data[2])
-    head_reach = math.sqrt(head.data[1] * head.data[1] + head.data[2] * head.data[2])
-
-    if head_reach > node_reach:
-        node.link = head
-        last = head
-        while last.link != head:
-            last = last.link
-
-        last.link = node
-        head = node
-        return
-
-    current = head
-    while current.link != head:
-        pre = current
-        current = current.link
-        curx = current.data[1]
-        cury = current.data[2]
-        cur_reach = math.sqrt(curx * curx + cury * cury)
-        if cur_reach > node_reach:
-            pre.link = node
-            node.link = current
-            return
-
-    current.link = node
-    node.link = head
 
 
 ## 전역 변수 선언 부분 ##
 memory = []
 head, current, pre = None, None, None
-name = []
+dataArray = ["다현", "정연", "쯔위", "사나", "지효"]
+
 ## 메인 코드 부분 ##
-if __name__ == "__main__":
+if __name__ == "__main__" :
 
-    store_array = []
-    store_start = 'A'
-    for _ in range(10):
-        store = (store_start, random.randint(1, 100), random.randint(1, 100))
-        store_array.append(store)
-        store_start = chr(ord(store_start) + 1)
+    node = Node()		# 첫 번째 노드
+    node.data = dataArray[0]
+    head = node
+    memory.append(node)
 
-    for store in store_array:
-        sort_store(store)
-    print_store(head)
+    for data in dataArray[1:] :	# 두 번째 이후 노드
+        pre = node
+        node = Node()
+        node.data = data
+        pre.nlink = node
+        node.plink = pre
+        memory.append(node)
+
+    printNodes(head)
