14a06e6fe470beeb3b9edb6060acab1ad1babb58
KimTaeYeong <12170548@inha.edu>
2023-02-09 22:03:09 +0900

    day19_mission03

--- day19_mission.py
+++ day19_mission.py
@@ -1,24 +1,15 @@
-from tkinter import *
+def score_sort(ary):
+    n = len(ary)
+    for end in range(1, n):
+        for i in range(end, 0, -1):
+            if ary[i-1][1] > ary[i][1] :
+                ary[i-1], ary[i] = ary[i], ary[i-1]
+    return ary
 
-## 함수 선언 부분 ##
-def draw_triangle(x, y, size) :
-    if size >= 30 :
-        draw_triangle(x, y, size/2)
-        draw_triangle(x+size/2, y, size / 2)
-        draw_triangle(x + size / 4, int(y-size*(3**0.5)/4), size / 2)
-    else :
-        canvas.create_polygon (x, y, x + size, y, x + size / 2, y - size*(3 ** 0.5) / 2, fill = 'red', outline = "red")
+ex_array = [['선미', 88], ['초아', 99], ['화사', 71], ['영탁', 78], ['영웅', 67], ['민호', 92]]
 
-## 전역 변수 선언 부분 ##
-w_size = 1000
-radius = 400
+if __name__ == "__main__":
+    print(f'정렬 전 --> {ex_array}')
+    print(f'정렬 후 --> {score_sort(ex_array)}')
 
-## 메인 코드 부분 ##
-window = Tk()
-window.title("삼각형 모양의 프랙탈")
-canvas = Canvas(window, height = w_size, width = w_size, bg = 'white')
 
-draw_triangle(w_size/5, w_size/5*4, w_size*2/3)
-
-canvas.pack()
-window.mainloop()
